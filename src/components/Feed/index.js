b1c1e622cdc057497f7c79260a97eb82ce42f5b9
Judamaro <juan.mayorga@uao.edu.co>
2021-04-05 17:06:23 -0500

    Component Post 1.0
    Compoente post
    *Primera version
    -Revisar espacios, iconos

--- src/components/Feed/index.js
+++ src/components/Feed/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { InputIcon } from "./inputIcon";
+import { Post } from "./post";
 
 import CreateIcon from "@material-ui/icons/Create";
 import PhotoIcon from "@material-ui/icons/Photo";
@@ -35,18 +36,9 @@ export const Feed = () => {
         </div>
         <div className="feed_input_icons">
           {/*ICONS*/}
-          <InputIcon 
-            Icon={PhotoIcon} 
-            Title="Foto" 
-            Color="#70b5f9" />
-          <InputIcon 
-            Icon={YouTubeIcon} 
-            Title="Video" 
-            Color="#7fc15e" />
-          <InputIcon 
-            Icon={EventIcon} 
-            Title="Evento" 
-            Color="#e7a33e" />
+          <InputIcon Icon={PhotoIcon} Title="Foto" Color="#70b5f9" />
+          <InputIcon Icon={YouTubeIcon} Title="Video" Color="#7fc15e" />
+          <InputIcon Icon={EventIcon} Title="Evento" Color="#e7a33e" />
           <InputIcon
             Icon={VerticalSplitIcon}
             Title="Escribir Articulo"
@@ -54,6 +46,35 @@ export const Feed = () => {
           />
         </div>
       </div>
+      {/*POST*/}
+      <Post
+        name="Judamaro"
+        description="Componete post de prueba"
+        message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        photoURL="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+        images="https://www.acueducto.com.co/guatoc/Archivos/resources/seccion_gerente/images/Swan_large.jpg"
+      />
+      <Post
+        name="Judamaro"
+        description="Componete post de prueba"
+        message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        photoURL="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+        images="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+      />
+      <Post
+        name="Judamaro"
+        description="Componete post de prueba"
+        message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        photoURL="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+        images="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+      />
+      <Post
+        name="Judamaro"
+        description="Componete post de prueba"
+        message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        photoURL="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270"
+        images="https://www.istockphoto.com/resources/images/HomePage/Hero/950473038.jpg"
+      />
     </div>
   );
 };
---
+++ src/components/Feed/post.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Avatar } from "@material-ui/core";
+import "./styles.css";
+import { InputIcon } from "./inputIcon";
+
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
+
+export const Post = ({ name, description, message, photoUrl, images }) => {
+  return (
+    <div className="post">
+      <div className="post-header">
+        <Avatar src={photoUrl} />
+        <div className="post-info">
+          <h2>{name}</h2>
+          <p>{description}</p>
+        </div>
+      </div>
+      <div className="post-body">
+        <p>{message}</p>
+        {images ? <img src={images} /> : ""}
+      </div>
+      <div className="post-buttom">
+          <InputIcon Icon={ThumbUpAltOutlinedIcon} Title="Recomendar" Color='gray'/>
+          <InputIcon Icon={ChatOutlinedIcon} Title="Comentar" Color='gray'/>
+          <InputIcon Icon={ShareOutlinedIcon} Title="Compartir" Color='gray'/>
+          <InputIcon Icon={SendOutlinedIcon} Title="Enviar" Color='gray'/>
+      </div>
+    </div>
+  );
+};
