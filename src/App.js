f3c8e4a9dfd224b8646c2ecab04d5fe5c47a4bf8
Judamaro <juan.mayorga@uao.edu.co>
2021-04-06 19:37:55 -0500

    Componete Login / Registro
    Componentes Login y Registro Completos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Feed } from "./components/Feed";
 import { Header } from "./components/Header";
+import { Login } from "./components/Login";
 import { Sidebar } from "./components/Sidebar";
 import { Widgets } from "./components/Widget";
 
 function App() {
+  const [user, setUser] = useState(false);
+
   return (
     <div className="App">
       <Header></Header>
-      <div className="wrapper">
-        {/*Sidebar Component*/}
-        <Sidebar />
-        {/*Feed Component*/}
-        <Feed />
-        {/*Widgets Component*/}
-        <Widgets/>
-      </div>
+
+      {user ? (
+        <div className="wrapper">
+          {/*Sidebar Component*/}
+          <Sidebar />
+          {/*Feed Component*/}
+          <Feed />
+          {/*Widgets Component*/}
+          <Widgets />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
