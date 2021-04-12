3fe6c3152d6e9949a83a2273ede2aa9f35d6bb89
Judamaro <juan.mayorga@uao.edu.co>
2021-04-12 16:22:31 -0500

    Actualizacion foto en el componente Header
    Actualizacion foto(avatar) del usuario en el componente header

--- src/components/Header/headerIcons.js
+++ src/components/Header/headerIcons.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Avatar } from '@material-ui/core';
+import { selectUser } from '../../features/useSlice';
+import { useSelector } from 'react-redux';
+
 import './styles.css'
 
 export const HeaderIcons = ({Icon, Title, Text, Profile, Logout}) => {
+
+    const user = useSelector(selectUser)
+
     return (
         <div className="header_icon" onClick={Logout}>
             {Icon && <Icon className="icon_"/>}
-            {Profile && <Avatar className="profile_"/>}
+            {Profile && (user ? <Avatar src={user.photoURL} className="profile_">{user.email[0]}</Avatar> : <Avatar className="profile_"/>)}
             {Title ? <h3>{Title}</h3> : <h3 className="text_">{Text}</h3>}           
         </div>
     );
